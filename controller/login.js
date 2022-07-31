d199a2ead9d4324288a2a0a006a2788cc8705f20
alangim <82983554+kartucho@users.noreply.github.com>
2022-07-31 17:11:56 -0300

    login

---
+++ controller/login.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const router = require('express').Router();
+require('dotenv').config()
+
+router.post('/', (req, res) => {
+    const { body } = req
+    if (body.username == 'master' && body.password == 'ifts16') {
+        //usuario correcto
+        const tokenData = {
+            username: body.username,
+            id: 1,
+            profile: 'Admin Master'
+        }
+
+        const token = jwt.sign(tokenData, process.env.JWTSECRET, { expiresIn: '1h' })
+        res.status(200).send({ token, name: 'Master of the universe' })
+    } else {
+        //usuario incorrecto
+        return res.status(401).json({ error: 'credenciales incorrectas' })
+    }
+})
+
+module.exports = router
